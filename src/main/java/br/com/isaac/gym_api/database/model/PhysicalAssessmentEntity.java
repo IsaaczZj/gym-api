3265c5056298fe9970f86385cf584ea4706b5550
isaac_dev <joseisaac.isaac123@gmail.com>
2026-06-24 13:29:00 -0300

    feat:criando entidade de avaliacao fisica

--- src/main/java/br/com/isaac/gym_api/database/model/PhysicalAssessmentEntity.java
+++ src/main/java/br/com/isaac/gym_api/database/model/PhysicalAssessmentEntity.java
@@ -1,4 +1,32 @@
 package br.com.isaac.gym_api.database.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "physical_assessments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class PhysicalAssessmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal weight;
+
+    @Column(nullable = false)
+    private BigDecimal height;
+
+    @Column(name = "body_fat_percentage", nullable = false)
+
+    private BigDecimal bodyFatPercentage;
+
 }
